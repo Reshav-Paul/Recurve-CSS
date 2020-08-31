c0a035fb16a0fb0a1b2da56fdf9e8a143c10309f
Reshav Paul <paulrishav65@gmail.com>
2020-08-31 01:59:43 -0700

    Completed documentation upto row-cols section

--- docs/src/component-creators.js
+++ docs/src/component-creators.js
@@ -16,8 +16,78 @@ let Component = (function() {
         headerListItem.appendChild(headerLink);
         return headerListItem;
     }
+
+    function getStyledParagraph(textGroup) {
+        const para = document.createElement('p');
+        for (const text of textGroup) {
+            const span = document.createElement('span');
+            span.textContent = `${text} `;
+            if (text.startsWith('.')) {                    
+                span.classList.add('text-red');
+            }
+            para.appendChild(span);
+        }
+        return para;
+    }
+
+    function createUnorderedList(listTextGroups) {
+        const uList = document.createElement('ul');
+        for (const textGroup of listTextGroups) {
+            const listItem = document.createElement('li');
+            for (const text of textGroup) {
+                const span = document.createElement('span');
+                span.textContent = text;
+                if (text.startsWith('.')) {                    
+                    span.classList.add('text-red');
+                }
+                listItem.appendChild(span);
+            }
+            uList.appendChild(listItem);
+        }
+        return uList;
+    }
     
-    return {navHeaderLink};
+    function greenSpan(text, classlist) {
+        const span = document.createElement('span');
+        span.textContent = text;
+        span.classList.add(...classlist);
+        span.classList.add('text-green');
+        return span;
+    }
+
+    function getRow(rowProps) {
+        const row = document.createElement('div');
+        row.classList.add('row', 'my-2');
+        rowProps.classList && row.classList.add(...rowProps.classList);
+        console.log(rowProps.cols);
+        for (const child of rowProps.cols) {
+            const col = document.createElement('div');
+            col.style.fontSize = '0.9rem';
+            if (!child.text) {
+                col.textContent = child.classList.map(name => `.${name}`).join(' ');
+            } else {
+                col.textContent = child.text;
+            }
+            col.classList.add(...child.classList);
+            col.classList.add('py-3', 'bg-col');
+            row.appendChild(col);
+        }
+        return row;
+    }
+
+    function getContainerWithRows(rows) {
+        const container = document.createElement('div');
+        container.classList.add('container');
+        for (const row of rows) {
+            container.appendChild(getRow(row));
+        }
+        return container;
+    }
+
+    return {
+        navHeaderLink, createUnorderedList, greenSpan,
+        getRow, getStyledParagraph, getContainerWithRows
+    };
 })();
 
 export default Component;
--- docs/src/content.js
+++ docs/src/content.js
@@ -1,7 +1,11 @@
+import Component from './component-creators';
+
 function getHeading(id, text) {
     const heading = document.createElement('h3');
     heading.classList.add('section-header');
-    heading.id = id;
+    if (id.length > 0) {
+        heading.id = id;
+    }    
     heading.textContent = text;
     return heading;
 }
@@ -25,6 +29,131 @@ function getParagraph(text, classnames = []) {
 const content = document.getElementById('content');
 
 // Grid Layout
-let gridLayoutHeading = getHeading('grid-layout', 'Grid Layout');
-let containerSubheading = getSubheading('container', 'Containers');
+const gridLayoutHeading = getHeading('grid-layout', 'Grid Layout');
 content.appendChild(gridLayoutHeading);
+
+const containerSubheading = getSubheading('container', 'Containers');
+const description = 'Containers are the building blocks of the grid layout. They contain the rows that construct the grid. They also contain your content so that you can apply the required margin, padding and alignment.';
+const containerDesc = getParagraph(description, ['text-grey', 'h5']);
+content.appendChild(containerSubheading);
+content.appendChild(containerDesc);
+
+const containerFeatures = Component.createUnorderedList(
+    [
+        [
+            'The class ',
+            '.container ',
+            'is responsive across all viewports and sets a max-width at each breakpoint'
+        ],
+        [
+            'The class ',
+            '.container-fluid ',
+            'is always 100% wide with respect to its parent'
+        ],
+        [
+            '.container-[breakpoint] ',
+            'are 100% wide upto the specified breakpoint'
+        ]
+    ]
+);
+content.appendChild(containerFeatures);
+content.appendChild(getHeading('row-columns', 'Rows and Columns'));
+const rowColDesc = getParagraph(
+    'Rows are contained within containers. Each row can display 12 columns by default. The number of columns can be changed by using the sass variable ',
+    ['h5', 'text-grey']
+);
+rowColDesc.appendChild(Component.greenSpan('$grid-columns', ['h5']));
+content.appendChild(rowColDesc);
+
+content.appendChild(getSubheading('', 'Equal width columns'));
+content.appendChild(Component.getStyledParagraph([ 'Using the class', '.col', 'creates equal width columns' ]));
+content.appendChild(Component.getContainerWithRows(
+    [
+        { cols: [{classList: ['col']}, {classList: ['col']}, {classList: ['col']}] },
+        { cols: [{classList: ['col']}, {classList: ['col']}] }
+    ]    
+));
+
+content.appendChild(getSubheading('', 'Setting column widths'));
+content.appendChild(Component.getStyledParagraph(
+    [
+        'Classes',
+        '.col-[size]',
+        'can be used to create columns of specific sizes based on the maximum number of columns in the grid system '
+    ]
+));
+content.appendChild(Component.getContainerWithRows(
+    [
+        { cols: [{classList: ['col']}, {classList: ['col-6']}, {classList: ['col']}] },
+        { cols: [{classList: ['col-4']}, {classList: ['col-5']}, {classList: ['col']}] },
+        { cols: [{classList: ['col-8']}, {classList: ['col-4']}] },
+        { cols: [{classList: ['col-2']}, {classList: ['col-6']}, {classList: ['col-2']}] }
+    ]    
+));
+
+content.appendChild(getSubheading('', 'Responsive columns'));
+content.appendChild(Component.getStyledParagraph(
+    [
+        'Classes',
+        '.col-[breakpoint]-[size]',
+        'can be used to change the column width at each breakpoint. Since recurve has a desktop-first design approach, the default',
+        '.col-[responsive]-[size]',
+        'classes specify the size of the column for the specified breakpoint and downwardsResponsive columns can be mixed with',
+        '.col',
+        'columns and auto-width columns to achieve the required design.'
+    ]
+));
+content.appendChild(Component.getContainerWithRows(
+    [
+        { cols: [{classList: ['col']}, {classList: ['col-4', 'col-md-6']}] },
+        { cols: [{classList: ['col-4', 'col-md-6']}, {classList: ['col-4', 'col-md-6']}, {classList: ['col-4', 'col-md-6']}] },
+        { cols: [{classList: ['col-md-6']}, {classList: ['col-md-6']}] },
+        {
+            cols: [
+                {classList: ['col-8', 'col-lg-6', 'col-md-4', 'col-sm-3', 'col-xs-12']},
+                {classList: ['col-4', 'col-lg-6', 'col-md-8', 'col-sm-9', 'col-xs-12']}
+            ]
+        },
+        { cols: [{classList: ['col-6']}, {classList: ['col-6', 'col-md-8']}] },        
+    ]    
+));
+
+content.appendChild(getSubheading('', 'Responsive auto width columns'));
+content.appendChild(Component.getStyledParagraph(
+    [
+        '.col-auto',
+        'and',
+        '.col-[breakpoint]-[auto]',
+        'classes can be used to create auto-width columns which have the width property set to auto',
+        '.col-[breakpoint]-[auto]',
+        'makes the width auto for screens of that size and smaller, while',
+        '.col-auto',
+        'is the default and applies for all screens.'
+    ]
+));
+content.appendChild(Component.getContainerWithRows(
+    [
+        { cols: [{classList: ['col-2', 'col-lg-3']}, {classList: ['col-6', 'col-lg-auto']}, {classList: ['col-auto']}] },
+        { cols: [{classList: ['col-6', 'col-md-auto']}, {classList: ['col-6']}] }
+    ]    
+));
+
+content.appendChild(getSubheading('', 'Row Columns'));
+content.appendChild(Component.getStyledParagraph(
+    [
+        'Using the classes',
+        '.row-col-*',
+        'and their responsive variants',
+        '.col-row-[breakpoint]-*',
+        ' you can quickly set the number of columns as a shortcut directly along with the row instead of specifying the sizes on each column.'
+    ]
+));
+const rowCol = Component.getContainerWithRows(
+    [
+        {
+            classList: ['row-cols-3'],
+            cols: [{classList: ['col']}, {classList: ['col']}, {classList: ['col']}, {classList: ['col']}]
+        },
+    ],
+);
+content.appendChild(rowCol);
